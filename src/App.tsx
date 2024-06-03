c509d262dc8b6e8e55884ef6825f3ab3bf6ba156
Mladen Stepanic <crawleyprint@gmail.com>
2024-06-03 21:02:09 +0200

    Reformat code, add unicorn

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
-import './App.css'
-import Dialog from './components/Dialog'
+import "./App.css";
+import Dialog from "./components/Dialog";
 
 function App() {
   return (
@@ -7,17 +7,16 @@ function App() {
       <main className="content">
         <h1>HTML native dialogs</h1>
         <p>
-          Dialogs are nice. They are a part of the HTML standard,
-          easy to create and accessible. You can use keyboard to 
-          navigate them with very little effort and they, for the 
-          most part, work without javascript.
+          Dialogs are nice. They are a part of the HTML standard, easy to create
+          and accessible. You can use keyboard to navigate them with very little
+          effort and they, for the most part, work without javascript.
         </p>
         <div className="dialog-value">
           This text will change when you update the dialog.
         </div>
-        <Dialog 
-          onClose={() => console.log('close dialog')} 
-          targetLabel='Click to open dialog'
+        <Dialog
+          onClose={() => console.log("close dialog")}
+          targetLabel="Click to open dialog"
         >
           <header>
             <h1>Dialog title</h1>
@@ -25,8 +24,8 @@ function App() {
           <form method="dialog">
             <main>
               <label htmlFor="text">
-                Type in the text below and it will be shown when you 
-                close the dialog
+                Type in the text below and it will be shown when you close the
+                dialog
                 <input type="text" />
               </label>
             </main>
@@ -34,7 +33,7 @@ function App() {
         </Dialog>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,47 +1,47 @@
-import { useRef } from 'react';
-import type { FC, ReactNode } from 'react';
+import { useRef } from "react";
+import type { FC, ReactNode } from "react";
 
 type DialogProps = {
-    targetLabel: string;
-    closeBtnLabel?: string;
-    onClose?: () => void;
-    children?: ReactNode;
+  targetLabel: string;
+  closeBtnLabel?: string;
+  onClose?: () => void;
+  children?: ReactNode;
 };
-const Dialog: FC<DialogProps> = ({ 
-    targetLabel, 
-    closeBtnLabel = 'Close', 
-    children, 
-    onClose, 
+const Dialog: FC<DialogProps> = ({
+  targetLabel,
+  closeBtnLabel = "Close",
+  children,
+  onClose,
 }) => {
-    const dialogRef = useRef<HTMLDialogElement>(null);
-    function showDialog() {
-        if (!dialogRef?.current) return;
-        dialogRef.current.addEventListener('close', onDialogClose);
-        dialogRef.current.showModal();
-    }
-    function onDialogClose() {
-        onClose?.();
-        dialogRef?.current?.close();
-        dialogRef?.current?.removeEventListener('close', onDialogClose);
-    }
-    return (
-        <>
-            <button 
-                type="button" 
-                onClick={showDialog}
-                className="btn dialog-open" 
-                tabIndex={0}>{targetLabel}
-            </button>
-            <dialog ref={ dialogRef } className="dialog">
-                {children}
-                <button 
-                    tabIndex={0} 
-                    className="btn" 
-                    onClick={onDialogClose}
-                >{closeBtnLabel}</button>
-            </dialog>
-        </>
-    )
-}
+  const dialogRef = useRef<HTMLDialogElement>(null);
+  function showDialog() {
+    if (!dialogRef?.current) return;
+    dialogRef.current.addEventListener("close", onDialogClose);
+    dialogRef.current.showModal();
+  }
+  function onDialogClose() {
+    onClose?.();
+    dialogRef?.current?.close();
+    dialogRef?.current?.removeEventListener("close", onDialogClose);
+  }
+  return (
+    <>
+      <button
+        type="button"
+        onClick={showDialog}
+        className="btn dialog-open"
+        tabIndex={0}
+      >
+        {targetLabel}
+      </button>
+      <dialog ref={dialogRef} className="dialog">
+        {children}
+        <button tabIndex={0} className="btn" onClick={onDialogClose}>
+          {closeBtnLabel}
+        </button>
+      </dialog>
+    </>
+  );
+};
 
 export default Dialog;
