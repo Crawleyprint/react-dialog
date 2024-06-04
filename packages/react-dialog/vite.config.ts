6d9774f5bdca248beeda1f89fe7e85563d7b8578
Mladen Stepanic <crawleyprint@gmail.com>
2024-06-04 20:00:21 +0200

    Fix vite config for lib

--- packages/react-dialog/vite.config.ts
+++ packages/react-dialog/vite.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
   // Configuration for building your library.
   // See: https://vitejs.dev/guide/build.html#library-mode
   build: {
-    outDir: '../../dist/packages/react-dialog',
+    outDir: './dist/',
     emptyOutDir: true,
     reportCompressedSize: true,
     commonjsOptions: {
