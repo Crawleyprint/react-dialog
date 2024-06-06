7e7f61b2462475dffc50d9621d4eae192670a3bd
Mladen Stepanic <crawleyprint@gmail.com>
2024-06-06 16:25:20 +0200

    Remove basePath from hash router

--- packages/homepage/src/main.tsx
+++ packages/homepage/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react';
 import * as ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 
 import App from './app/app';
 
@@ -9,8 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <StrictMode>
-    <BrowserRouter basename="/react-dialog">
+    <HashRouter>
       <App />
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
