1bdb7782b162efdad08467d6e312f9b554e83cc1
Mladen Stepanic <crawleyprint@gmail.com>
2024-06-09 10:17:48 +0200

    Re-enable all tests

--- packages/react-dialog/src/lib/hooks/useDropdown.spec.ts
+++ packages/react-dialog/src/lib/hooks/useDropdown.spec.ts
@@ -1,13 +1,14 @@
-import { screen, renderHook, act } from '@testing-library/react';
+import { renderHook, act } from '@testing-library/react';
 import { useDropdown } from './useDropdown';
 import userEvent from '@testing-library/user-event';
 
 describe('useDropdown()', () => {
-  it.skip('should switch to being closed when clicked outside of the container', async () => {
+  it('should switch to being closed when clicked outside of the container', async () => {
     const user = userEvent.setup();
     const dialog = document.createElement('dialog');
+    const anchor = document.createElement('span');
     const { result } = renderHook(useDropdown, {
-      initialProps: { dialog },
+      initialProps: { dialog, anchor },
     });
     act(() => {
       result.current.openDialog();
