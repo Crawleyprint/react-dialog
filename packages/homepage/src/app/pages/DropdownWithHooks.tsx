362a2db7c65281aa64a7f4c2c5f1ea01383eaf37
Mladen Stepanic <crawleyprint@gmail.com>
2024-06-09 14:35:15 +0200

    add example hook component to homepage

--- packages/homepage/src/app/pages/DropdownWithHooks.tsx
+++ packages/homepage/src/app/pages/DropdownWithHooks.tsx
@@ -1,42 +1,25 @@
-import { useRef, useState, useEffect } from 'react';
+import { useRef } from 'react';
 import { useDropdown } from '@crawleyprint/react-dialog'; // Make sure the path is correct
 import css from '../app.module.css';
 
 const DropdownWithHooks = () => {
   const dialogRef = useRef(null);
   const anchorRef = useRef(null);
-  const [isOpen, setIsOpen] = useState(false);
 
-  const { openDialog, closeDialog, open, floatingStyles } = useDropdown({
+  const { open, openDialog, closeDialog, floatingStyles } = useDropdown({
     dialog: dialogRef.current,
     anchor: anchorRef.current,
-    isOpen,
-    floating: { placement: 'bottom' },
+    floating: { placement: 'bottom', middleware: [] },
   });
 
-  useEffect(() => {
-    if (isOpen) {
-      openDialog();
-    }
-  }, [isOpen, openDialog]);
-
-  const showDialog = () => {
-    setIsOpen(true);
-  };
-
-  const closeDropdown = () => {
-    closeDialog();
-    setIsOpen(false);
-  };
-
   return (
-    <div>
-      <button ref={anchorRef} onClick={showDialog}>
+    <div className={`${open ? ' dropdown--open' : ''}`}>
+      <button tabIndex={0} ref={anchorRef} onClick={openDialog}>
         Show Dropdown
       </button>
       <dialog
         ref={dialogRef}
-        className={`${css.dropdown}${open ? ' dropdown--open' : ''}`}
+        className={`${css.dropdown}`}
         style={{
           margin: 0,
           ...floatingStyles,
@@ -47,7 +30,9 @@ const DropdownWithHooks = () => {
           <li>Item 2</li>
           <li>Item 3</li>
         </ul>
-        <button onClick={closeDropdown}>Close dropdown</button>
+        <button tabIndex={0} onClick={closeDialog}>
+          Close dropdown
+        </button>
       </dialog>
     </div>
   );
