947fad60991b7c8755dc462fcf2f3b3d4efc8548
Mladen Stepanic <crawleyprint@gmail.com>
2024-06-22 16:55:36 +0200

    Add more info to homepage

--- packages/homepage/src/app/app.tsx
+++ packages/homepage/src/app/app.tsx
@@ -26,28 +26,13 @@ export function App() {
         <h1>HTML native dialogs in React</h1>
       </header>
       <main className={styles.content}>
-        <section>
-          <p>
-            Dialogs are nice. They are a part of the HTML standard, easy to
-            create and accessible. You can use keyboard to navigate them with
-            very little effort and they, for the most part, work without
-            javascript.
-          </p>
-          <p>
-            However, most of the React dialog libraries are not using native
-            dialog element, mostly because it's a fairly recent addition to the
-            standard. They rely on various hacks instead, most of them involving
-            portals.
-          </p>
-          <p>
-            The trouble with portals is that they are usually rendered at the
-            end of the DOM, which hurts accessibility very badly. In order to
-            restore it, react dialogs and modals are going further down the
-            hacky rabbit hole and use various focus traps which often don't work
-            as intended.
-          </p>
-        </section>
         <nav className={styles.navigation} role="navigation">
+          <p>
+            <strong>
+              TLDR; Dialogs are awesome. Here are some examples for you.{' '}
+            </strong>
+            If you're interested in why this library was made, read on.
+          </p>
           <h2>Examples</h2>
           {navRoutes.map(({ url, title }, index) => (
             <>
@@ -70,14 +55,122 @@ export function App() {
             </>
           ))}
         </nav>
-        <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/dropdown" element={<DropdownPage />} />
-          <Route path="/embed-elements" element={<EmbedAnything />} />
-          <Route path="/open-by-default" element={<AlwaysOpen />} />
-          <Route path="/hooks" element={<StandaloneHooks />} />
-          <Route path="/dropdown-with-hooks" element={<DropdownWithHooks />} />
-        </Routes>
+        <div className="examples">
+          <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/dropdown" element={<DropdownPage />} />
+            <Route path="/embed-elements" element={<EmbedAnything />} />
+            <Route path="/open-by-default" element={<AlwaysOpen />} />
+            <Route path="/hooks" element={<StandaloneHooks />} />
+            <Route
+              path="/dropdown-with-hooks"
+              element={<DropdownWithHooks />}
+            />
+          </Routes>
+        </div>
+        <section>
+          <h2>Reasoning</h2>
+          <p className={styles['initial-letter']}>
+            Dialogs are pretty great. They’re part of the{' '}
+            <a
+              href="https://html.spec.whatwg.org/multipage/interactive-elements.html#the-dialog-element"
+              target="_blank"
+            >
+              HTML standard
+            </a>
+            , included in{' '}
+            <a
+              href="https://web.dev/blog/introducing-baseline?hl=en"
+              target="blank"
+            >
+              Baseline 2022
+            </a>
+            , easy to create, and super accessible. You can navigate them with
+            your keyboard without much hassle, and they mostly work without
+            JavaScript.
+          </p>
+          <p>
+            But here's the thing: most React dialog libraries don't use the
+            native dialog element because it's a relatively new addition to the
+            standard. Instead, they use various hacks, often involving portals.
+          </p>
+          <p>
+            The problem with portals is that they usually get rendered at the
+            end of the DOM, which messes up accessibility. To fix this, React
+            dialogs and modals dive deeper into hacky solutions, using focus
+            traps that often don’t work as well as they should.
+          </p>
+          <h2>Benefits of using native dialogs in React</h2>
+          <p>
+            Using native dialog elements in React could simplify things a lot.
+            They come with built-in features like automatic focus trapping, easy
+            keyboard navigation, and a customizable backdrop out of the box.
+            This means you get a background behind the dialog to focus attention
+            on it without additional coding. Moreover, native dialogs don’t
+            struggle with <code>overflow: hidden</code> and <code>z-index</code>{' '}
+            issues because they are rendered on top of all other elements,
+            ensuring they are always visible and accessible.​
+          </p>
+          <h2>Installation and usage</h2>
+          <p>
+            So, you're onboard the idea and want to try this? Great! Check it
+            out on
+            <a
+              href="https://github.com/Crawleyprint/react-dialog"
+              target="_blank"
+            >
+              Github
+            </a>{' '}
+            and{' '}
+            <a
+              href="https://www.npmjs.com/package/@crawleyprint/react-dialog"
+              target="_blank"
+            >
+              npm
+            </a>
+            . Don't hesitate to open an issue if you try it and find it's
+            missing something you'd need.
+          </p>
+          <h2>Credit where credit's due</h2>
+          <p>
+            While I was working on this library, I've used the works of others,
+            mostly from these sources:
+            <ol>
+              <li>
+                <a
+                  href="https://developer.mozilla.org/en-US/docs/Web/HTML/Element/dialog"
+                  target="_blank"
+                >
+                  MDN Web Docs on dialog element
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://css-tricks.com/dialog-components-roll-your-own/"
+                  target="_blank"
+                >
+                  Rob Levin's excellent article on CSS Tricks
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://html.spec.whatwg.org/multipage/interactive-elements.html#the-dialog-element"
+                  target="_blank"
+                >
+                  HTML Spec for the dialog element
+                </a>
+              </li>
+              <li>
+                <a
+                  href="https://github.com/sjc5/use-html-dialog"
+                  target="_blank"
+                >
+                  This hook by Samuel J. Cook
+                </a>
+              </li>
+            </ol>
+          </p>
+        </section>
       </main>
       <footer>
         <section>
