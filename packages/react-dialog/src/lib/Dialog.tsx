5d57ef39d4ae4c45418cb17d4983766aee9720d8
Mladen Stepanic <crawleyprint@gmail.com>
2025-05-04 13:37:52 +0200

    Memoize Dialog component to prevent unnecessary re-renders

--- packages/react-dialog/src/lib/Dialog.tsx
+++ packages/react-dialog/src/lib/Dialog.tsx
@@ -1,4 +1,4 @@
-import { useRef } from 'react';
+import React, { useRef } from 'react';
 import type { FC } from 'react';
 
 import css from './react-dialog.module.css';
@@ -7,57 +7,59 @@ import { type DialogProps } from './types';
 
 const defaultStyle = {};
 
-export const Dialog: FC<DialogProps> = ({
-  targetLabel,
-  closeBtnLabel = 'Close',
-  children,
-  onClose,
-  isOpen = false,
-  style = defaultStyle,
-}) => {
-  const dialogRef = useRef<HTMLDialogElement>(null);
+export const Dialog: FC<DialogProps> = React.memo(
+  ({
+    targetLabel,
+    closeBtnLabel = 'Close',
+    children,
+    onClose,
+    isOpen = false,
+    style = defaultStyle,
+  }) => {
+    const dialogRef = useRef<HTMLDialogElement>(null);
 
-  const { openDialog, closeDialog } = useDialog({
-    dialog: dialogRef.current,
-    style,
-    isOpen,
-  });
+    const { openDialog, closeDialog } = useDialog({
+      dialog: dialogRef.current,
+      style,
+      isOpen,
+    });
 
-  function showDialog() {
-    openDialog();
-  }
+    function showDialog() {
+      openDialog();
+    }
 
-  function hideDialog() {
-    onClose?.();
-    closeDialog();
-  }
-  return (
-    <>
-      <button
-        data-testid="dialog-trigger"
-        type="button"
-        onClick={showDialog}
-        className={`btn dialog-open}`}
-        tabIndex={0}
-      >
-        {targetLabel}
-      </button>
-      <dialog
-        style={{ ...style }}
-        data-testid="dialog-body"
-        ref={dialogRef}
-        className={`${css.dialog}`}
-      >
-        {children}
+    function hideDialog() {
+      onClose?.();
+      closeDialog();
+    }
+    return (
+      <>
         <button
+          data-testid="dialog-trigger"
+          type="button"
+          onClick={showDialog}
+          className={`btn dialog-open}`}
           tabIndex={0}
-          className="btn"
-          onClick={hideDialog}
-          data-testid="dialog-close"
         >
-          {closeBtnLabel}
+          {targetLabel}
         </button>
-      </dialog>
-    </>
-  );
-};
+        <dialog
+          style={{ ...style }}
+          data-testid="dialog-body"
+          ref={dialogRef}
+          className={`${css.dialog}`}
+        >
+          {children}
+          <button
+            tabIndex={0}
+            className="btn"
+            onClick={hideDialog}
+            data-testid="dialog-close"
+          >
+            {closeBtnLabel}
+          </button>
+        </dialog>
+      </>
+    );
+  },
+);
